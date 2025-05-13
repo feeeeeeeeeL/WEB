7bb1342a6844f3d1f5ea52c34546e29c4ddb08c0
feeeeeeeeeL <kirmas1905@gmail.com>
2025-05-14 00:31:10 +0300

    Create routes.py

---
+++ app/api/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from app.models import User
+
+bp = Blueprint('api', __name__)
+
+@bp.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@bp.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_dict())
